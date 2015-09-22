6e104c3ac1adcd1495f3468daccc40d15373b065
Andrew McMullen <amm216@lehigh.edu>
2015-09-21 21:21:25 -0400

    Adding UI

---
+++ app/src/main/java/UI/login/NumberAdder.java
@@ -0,0 +1,23 @@
+package ui.login;
+
+/**
+ * Created by Noel on 9/20/15.
+ */
+public class NumberAdder {
+
+    public static int addNumbers(int one, int two){
+        int x = one;
+        int y = two;
+        int z = x + y;
+        return z;
+    }
+
+    public static int substractNumbers(int one, int two){
+        int x = one;
+        int y = two;
+        int z = x - y;
+        return z;
+    }
+
+
+}
